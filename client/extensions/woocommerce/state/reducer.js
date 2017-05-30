e7cc1063f857361ee0035c033e61b11687cbff53
Kevin Killingsworth <kevin@redfenix.com>
2017-05-30 14:45:31 -0500

    WooCommerce: Make api-plan general purpose
    This takes the api-plan code that was originally developed under
    state/ui/product and places it under state/api-plan. It also seeks to
    make more general use of the code and adds a README to explain its
    operation.
    
    This is really just the reducer/action code, there are still selectors
    to be written and more helper functions to be ported/written.

--- client/extensions/woocommerce/state/reducer.js
+++ client/extensions/woocommerce/state/reducer.js
@@ -4,10 +4,12 @@
 import ui from './ui/reducer';
 import { combineReducers } from 'state/utils';
 import wcApi from './wc-api/reducer';
+import apiPlan from './api-plan/reducer';
 import site from './site/reducer';
 
 export default combineReducers( {
 	ui,
 	wcApi,
+	apiPlan,
 	site,
 } );
--- client/extensions/woocommerce/state/ui/products/reducer.js
+++ client/extensions/woocommerce/state/ui/products/reducer.js
@@ -4,10 +4,8 @@
 import edits from './edits-reducer';
 import { combineReducers } from 'state/utils';
 import variations from './variations/reducer';
-import apiPlan from './api-plan/reducer';
 
 export default combineReducers( {
 	edits,
 	variations,
-	apiPlan,
 } );
