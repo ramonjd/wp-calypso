ccd51f18b8de9a883ea23edb11396c50fa02c37f
Kevin Killingsworth <kevin@redfenix.com>
2017-05-31 20:45:51 -0500

    Woo: Change API Plan to Action List
    This changes the name of the API Plan to Action List because "Plan"
    is an overloaded term within Calypso.

--- client/extensions/woocommerce/state/reducer.js
+++ client/extensions/woocommerce/state/reducer.js
@@ -4,12 +4,12 @@
 import ui from './ui/reducer';
 import { combineReducers } from 'state/utils';
 import wcApi from './wc-api/reducer';
-import apiPlan from './api-plan/reducer';
+import actionList from './action-list/reducer';
 import site from './site/reducer';
 
 export default combineReducers( {
 	ui,
 	wcApi,
-	apiPlan,
+	actionList,
 	site,
 } );
