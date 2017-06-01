5de1b50891df8e90b97d293fb64a560035c0081b
Kerry Liu <kerryliu@gmail.com>
2017-06-01 13:10:00 -0700

    Comments: select a site on filter paths

--- client/my-sites/comments/controller.js
+++ client/my-sites/comments/controller.js
@@ -3,18 +3,21 @@
  */
 import { renderWithReduxStore } from 'lib/react-helpers';
 import React from 'react';
+import { includes } from 'lodash';
+import page from 'page';
 
 /**
  * Internal dependencies
  */
 import route from 'lib/route';
 import CommentsManagement from './main';
+import controller from 'my-sites/controller';
+
+const STATUSES = [ 'pending', 'approved', 'trash', 'all', 'spam' ];
 
 export const comments = function( context ) {
 	const siteSlug = route.getSiteFragment( context.path );
-	const status = ( context.params.status && siteSlug !== context.params.status )
-		? context.params.status
-		: 'unapproved';
+	const status = context.params.status === 'pending' ? 'unapproved' : context.params.status;
 
 	renderWithReduxStore(
 		<CommentsManagement
@@ -25,4 +28,12 @@ export const comments = function( context ) {
 		'primary',
 		context.store
 	);
+};
+
+export const sites = function( context ) {
+	const { site } = context.params;
+	if ( ! includes( STATUSES, site ) ) {
+		return page.redirect( `/comments/pending/${ site }` );
+	}
+	controller.sites( context );
 };
